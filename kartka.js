931412d8a0728b3399426c3325e1ef45a67575fc
banjoaltf4 <123736395+banjoaltf4@users.noreply.github.com>
2023-01-27 12:30:06 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,45 @@
+// import kaboom lib
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// obrazki 
+
+loadSprite("kartka", "choinka.png")
+loadSprite("kometa", "kometa.png")
+loadSprite("napis", "napis.png")
+loadSound("cichanoc", "cichanoc.mp3")
+
+
+add([
+    sprite("kartka"),
+    pos(0, 0),
+])
+
+const kometa = add([
+    sprite("kometa"),
+    pos(400, 50),
+])
+
+onUpdate( ()=>{
+
+if(kometa.pos.x>50)
+
+    kometa.pos.x -=1
+
+else
+
+{
+    destroy(kometa)
+
+    add([
+        sprite("napis"),
+        pos(190, 150),
+    ])
+}
+})
+
+onKeyPress("space", ()=>{
+    play("cichanoc")
+})
